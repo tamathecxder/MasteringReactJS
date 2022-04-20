3af173ecf23286793556b82e090e4e9acb2518ae
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-20 15:08:43 +0700

    Bismillah

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const HelloWorld = () => {
+  return <p>Hello World</p>
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
   document.getElementById('root')
 );
