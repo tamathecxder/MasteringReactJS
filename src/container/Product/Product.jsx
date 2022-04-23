83fc7c37723affc5b933a92f7bcc9e88ceb7991a
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-23 16:58:10 +0700

    Cegah value minus saat menjalankan handler state minus

--- src/container/Product/Product.jsx
+++ src/container/Product/Product.jsx
@@ -4,6 +4,21 @@ class Product extends Component {
   state = {
     order: 4
   }
+
+  plusHandler = () => {
+    this.setState({
+      order: this.state.order + 1
+    })
+  }
+
+  minusHandler = () => {
+    if (this.state.order > 0 ) {
+      this.setState ({
+        order: this.state.order - 1
+      })
+    }
+  } 
+
   render() {
     return (
       <div className='container'>
@@ -11,7 +26,7 @@ class Product extends Component {
           <div className="col-md-5">
             <div className="card">
               <div className="card-header bg-white">
-                <d className="flex">
+                <div className="flex">
                   <div className='row'>
                     <div className="col-md-6 justify-content-start text-start">
                       <img src="https://www.pngitem.com/pimgs/m/369-3697938_surf-brand-logos-png-transparent-png.png" width={88} alt="logo" />
@@ -24,20 +39,20 @@ class Product extends Component {
                       </span>
                     </div>
                   </div>
-                </d>
-                <div className='brand-image'>
-                  <img src="https://pict-c.sindonews.net/dyn/620/pena/news/2021/04/28/123/411468/rog-zephyrus-duo-15-se-laptop-gaming-dua-layar-paling-bertenaga-ksg.jpg" className='img-thumbnail' alt="product_image" />
                 </div>
               </div>
               <div className="card-body">
+                <div className='brand-image mb-4'>
+                  <img src="https://pict-c.sindonews.net/dyn/620/pena/news/2021/04/28/123/411468/rog-zephyrus-duo-15-se-laptop-gaming-dua-layar-paling-bertenaga-ksg.jpg" className='img-thumbnail' alt="product_image" />
+                </div>
                 <p className="fw-bold card-title">Laptop Gaming Asus ROG Zephyrus NVIDIA 3050Ti</p>
                 <p>Rp 15.000.000</p>
-                <div className='counter'>
+                <div className='counter py-3'>
                   <div className="row justify-content-center">
                     <div className="col-md-6 d-flex">
-                      <button className="minus me-2 btn btn-primary"> - </button>
-                      <input className='form-control text-center' type="text" value={3} id="" />
-                      <button className="plus ms-2 btn btn-primary"> + </button>
+                      <button onClick={this.minusHandler} className="minus me-2 btn btn-primary"> - </button>
+                      <input className='form-control text-center' min={0} type="text" value={this.state.order} id="" />
+                      <button onClick={this.plusHandler} className="plus ms-2 btn btn-primary"> + </button>
                     </div>
                   </div>
                 </div>
