49385711a74ec88caf15c5ebda939a84979f45da
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-04-28 16:09:36 +0700

    Menangkap segala perubahan dalam form input dengan memasukkan event onChange

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -7,7 +7,18 @@ import axios from 'axios';
 
 class BlogPost extends Component {
   state = {
-    posts: []
+    posts: [],
+    blogForm: {
+      userId: 1,
+      id: ``,
+      title: ``,
+      author: ``,
+      body: ``
+    }
+  }
+
+  blogFormHandler = () => {
+    console.log('being typed...');
   }
 
   getPostAPI = () => {
@@ -29,14 +40,6 @@ class BlogPost extends Component {
   }
 
   componentDidMount() {
-    // fetch('https://jsonplaceholder.typicode.com/posts')
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     this.setState({
-    //       posts: json
-    //     })
-    //   });
-
     this.getPostAPI();
   }
 
@@ -52,42 +55,41 @@ class BlogPost extends Component {
                 <h1 className="fw-light">Simple Personal Blog</h1>
                 <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
                 <p>
-                  <button type="button" class="btn btn-primary my-2 me-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  <button type="button" className="btn btn-primary my-2 me-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     Make a simple post here
                   </button>
                   <a href="#" className="btn btn-secondary my-2">Secondary action</a>
-
-                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">Your posts</h5>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                          <div className="row">
-                            <div className="col-mb-6 text-start">
-                              <form>
-                                <div class="mb-3">
-                                  <label for="title" class="form-label">Post title</label>
-                                  <input type="text" class="form-control" id="title" aria-describedby="title" />
-                                </div>
-                                <div class="mb-3">
-                                  <label for="desc" class="form-label">Description</label>
-                                  <textarea class="form-control" id="desc" rows="3"></textarea>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                              </form>
-                            </div>
+                </p>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Your posts</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div className="modal-body">
+                        <div className="row">
+                          <div className="col-mb-6 text-start">
+                            <form>
+                              <div className="mb-3">
+                                <label htmlFor="title" className="form-label">Post title</label>
+                                <input type="text" onChange={this.blogFormHandler} className="form-control" id="title" aria-describedby="title" />
+                              </div>
+                              <div className="mb-3">
+                                <label htmlFor="desc" className="form-label">Description</label>
+                                <textarea onChange={this.blogFormHandler} className="form-control" id="desc" rows="3"></textarea>
+                              </div>
+                              <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
                           </div>
                         </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
+                      </div>
+                      <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                       </div>
                     </div>
                   </div>
-                </p>
+                </div>
               </div>
             </div>
           </section>
