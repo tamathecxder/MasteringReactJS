35efad9b6526904502514987c6031c27522c3122
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-03 22:41:30 +0700

    Membangun contoh simple pengimplementasian redux dengan membuat store, dispatch, reducer dan subscription

--- redux.js
+++ redux.js
@@ -2,22 +2,35 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-  value: 0
+  value: 0,
+  age: 18
 }
 
-// Membuat Reducer ({Tasklist}suatu fungsi yang mana store ini adalah value, value tersebut bisa kita update hanya oleh reducer. sehingga tidak ada fungsi lain yang bisa mengupdate store selain reducer)
+// 2. Membuat Reducer ({Tasklist}suatu fungsi yang mana store ini adalah value, value tersebut bisa kita update hanya oleh reducer. sehingga tidak ada fungsi lain yang bisa mengupdate store selain reducer)
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_AGE':
+      return { ...state, age: state.age + 1 };
+    case 'CHANGE_VALUE':
+      return { ...state, value: action.newValue };
+    default:
+      return state;
+      break;
+  }
 }
-// Membuat Store (tempat dimana kita menaruh state agar nantinya bisa digunakan secara global)
+
+// 1. Membuat Store (tempat dimana kita menaruh state agar nantinya bisa digunakan secara global)
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-// Membuat Dispatcher/Action (proses pemnaggilan sebuah instruksi yang disediakan oleh reducer)
+// 4. Subscription (proses yang setiap ada perubahan pada store, subscribe ini akan terpanggil)
+store.subscribe(() => {
+  console.log('store change:', store.getState());
+});
 
-
-
-// Subscription (proses pemanggilan store yang diperlukan)
+// 3. Membuat Dispatcher/Action (proses pemanggilan sebuah instruksi yang disediakan oleh reducer)
+store.dispatch({type: 'ADD_AGE'});
+store.dispatch({type: 'CHANGE_VALUE', newValue: 10}); 
 
 
 
