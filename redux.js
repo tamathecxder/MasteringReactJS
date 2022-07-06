790b92aab260bf4694795b49c0ceb657cfd27e67
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-07-06 11:42:27 +0700

    Impelementasi action dispatcher yang memanggil tasklist dalam reducer pada redux js

--- redux.js
+++ redux.js
@@ -3,11 +3,25 @@ const createStore = redux.createStore;
 
 const initialState = {
   value: 0,
-  counter: 0
+  age: 0
 }
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+  if ( action.type === 'ADD_AGE' ) {
+    return {
+      ...state,
+      age: state.age + 1
+    }
+  }
+
+  if ( action.type === 'CHANGE_VALUE' ) {
+    return {
+      ...state, 
+      value: state.value + action.newValue
+    }
+  }
+  
   return state;
 }
 
@@ -17,5 +31,15 @@ console.log(store.getState());
 
 
 // Dispatch / Action Dispatcher
+store.dispatch({
+  type: 'ADD_AGE'
+});
+
+store.dispatch({
+  type: 'CHANGE_VALUE',
+  newValue: 100
+});
+
+console.log(store.getState()); // melihat isi initial state yang sudah dilakukan dispatch
 
 // Subscription
